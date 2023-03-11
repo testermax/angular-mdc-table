e4ec76ec059fef18f2d2e674eea39a34c5539e11
testermax <joachim.leite@hotmail.com>
2023-03-11 09:00:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mdc-table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-snsjne)
---
+++ src/app/table-footer-row-example.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+interface Transaction {
+  item: string;
+  cost: number;
+}
+
+/**
+ * @title Footer row table
+ */
+@Component({
+  selector: 'table-footer-row-example',
+  styleUrls: ['table-footer-row-example.scss'],
+  templateUrl: 'table-footer-row-example.html',
+})
+export class TableFooterRowExample {
+  displayedColumns: string[] = ['item', 'cost', 'menu'];
+  transactions: Transaction[] = [
+    { item: 'Beach ball', cost: 4 },
+    { item: 'Towel', cost: 5 },
+    { item: 'Frisbee', cost: 2 },
+    { item: 'Sunscreen', cost: 4 },
+    { item: 'Cooler', cost: 25 },
+    { item: 'Swim suit', cost: 15 },
+  ];
+
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.transactions
+      .map((t) => t.cost)
+      .reduce((acc, value) => acc + value, 0);
+  }
+}
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
